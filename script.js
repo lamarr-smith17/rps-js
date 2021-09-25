27b322e8af70a62620a3c9dc8beb6641c131d2b8
LaMarr Smith <ljsmith19@my.trine.edu>
2021-09-25 15:43:57 -0400

    Added JS script, figure out first step

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    const rock = 'Rock';
+    const paper = 'Paper';
+    const scissors = 'Scissors';
+    let choiceList = [];
+    let choice = choiceList.push(rock, paper, scissors);
+    return console.log(choice);
+};
+computerPlay();
