5837c59f3a6850094f3fa065f2805b0616258487
LaMarr Smith <ljsmith19@my.trine.edu>
2021-09-29 17:48:13 -0400

    Add in the looping next and you are done!

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function computerPlay() {
     };
 };
 computerPlay();
+
+function playGame(playerSelection, computerSelection) {
+    var playerSelection = prompt("Welcome to Rock-Paper-Scissors! Please enter your choice:")
+    var playerSelection = playerSelection.toLowerCase();
+    var computerSelection = computerPlay();
+    if (playerSelection == "rock" && computerSelection == "Rock"){
+        return "This is a tie game! Nice!";
+    }else if(playerSelection == "rock" && computerSelection == "Paper"){
+        return "You Lost... Paper beats rock :(";
+    }else if(playerSelection == "rock" && computerSelection == "Scissors"){
+        return "Nice one! Rock beats scissors!";
+    }else if(playerSelection == "paper" && computerSelection == "Rock"){
+        return "Good prediction! Paper beats rock!";
+    }else if(playerSelection == "paper" && computerSelection == "Paper"){
+        return "It's a tie game! Let's go again!";
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "Yikes, the computer got you! Scissors beats paper!"
+    }else if(playerSelection == "scissors" && computerSelection == "Rock"){
+        return "Ouch! Rock smashes scissors!";
+    }else if(playerSelection == "scissors" && computerSelection == "Paper"){
+        return "Good one! Scissors slashes paper!";
+    }else{
+        return "It's a tie!"
+    };
+};
+playGame(playerSelection,computerSelection);
